17af0c53739090a1a2956e9f96b2eecbc231f40a
Steven Doran <steven.doran@flatironschool.com>
2019-12-09 12:24:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  return Math.abs(distance - 42) 
+}
+
+function distanceFromHqInFeet(distance) {
+  return Math.abs((distance - 42) * 264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination)
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else if (distance >= 2500) {
+    return 'cannot travel that far'
+  }
+}
